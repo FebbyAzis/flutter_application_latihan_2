a1eb7f3e6e5f9691dc27c5bd2bc96d2a3fa88502
Febby Azis <febbyazis02@gmail.com>
2022-11-19 00:34:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) :super(key: key);
+  // ignore: constant_identifier_names
+  static const String_title = 'Flutter Turorial';
+
+  @override
+Widget build(BuildContext context){
+  return MaterialApp(
+    title: String_title,
+    home: Scaffold(
+      appBar: AppBar(title: const Text(String_title)),
+      body: const MyStatefulWidget(),
+      ),
+      );
+    }
+  }
+
+  class MyStatefulWidget extends StatefulWidget{
+    const MyStatefulWidget({Key? key}) :super(key: key);
+
+    @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  }
+
+  class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+    @override
+  Widget build (BuildContext context){
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Banner(
+          message: 'New Arrival',
+          location: BannerLocation.topStart,
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.yellow,
+            alignment: Alignment.center,
+            child: const Text('Some Item'),
+            ),
+          ),
+        ),
+        );
+  }
+  }
